f4b5addb0d30506b4e7851926a25673df5405ea1
nadbot <Daniel.Hamandouche@gmx.net>
2020-07-27 02:34:49 +0200

    Added function to send request if correct password

--- Keypad/Keypad.ino
+++ Keypad/Keypad.ino
@@ -1,3 +1,23 @@
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiGratuitous.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureAxTLS.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
 //www.elegoo.com
 //2016.12.9
 
@@ -28,6 +48,9 @@ char hexaKeys[ROWS][COLS] = {
 const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network to connect to
 const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
 
+const char* host = "192.168.4.1";
+const uint16_t port = 80;
+
 
 // For ESP8266 Microcontroller
 byte rowPins[ROWS] = {D1, D2, D3, D4}; 
@@ -36,10 +59,33 @@ byte colPins[COLS] = {D5, D6, D7, D8};
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
-String password = "976431";
+String passwordKeypad = "976431";
 
 void setup(){
   Serial.begin(115200);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+    /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 String entered = "";
@@ -55,14 +101,65 @@ void loop() {
     if(key == '#'){
       Serial.println("Pressed #");
       Serial.println(entered);
-      if(entered == password){
+      if(entered == passwordKeypad){
         Serial.println("Correct password");
+        send_request("open");
       }else {
         Serial.println("Wrong password");
       }
       entered = "";
     }
   }
+
+// send_request("open");
+}
+
+void send_request(String route){
+  Serial.print("connecting to ");
+  Serial.print(host);
+  Serial.print(':');
+  Serial.println(port);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+
+  // This will send a string to the server
+  Serial.println("sending data to server");
+  if (client.connected()) {
+//    client.println("hello from ESP8266");
+      client.println("GET /open HTTP/1.0");
+  }
+
+  // wait for data to be available
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      delay(60000);
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("receiving from remote server");
+  // not testing 'client.connected()' since we do not need to send data here
+  while (client.available()) {
+    char ch = static_cast<char>(client.read());
+    Serial.print(ch);
+  }
+
+  // Close the connection
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+
+  delay(300000); // execute once every 5 minutes, don't flood remote service
 }
 //void loop(){
 //  String enteredString = "";
